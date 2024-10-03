000dfdd8b11e7319a47fb0612336ac739b997898
Markus Gabriel <mgabr.git@gmail.com>
2024-10-03 22:13:49 +0200

    Add initial project from Spring Initializr

---
+++ src/main/java/com/github/mgabr/openapi_playground/OpenapiPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.github.mgabr.openapi_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mgabr/openapi_playground/OpenapiPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mgabr.openapi_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
